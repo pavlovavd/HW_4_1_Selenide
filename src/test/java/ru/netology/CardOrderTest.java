f6652a35d4776db7a9f01e1765ba600f70ca5796
Victoria Pavlova <pvd0510@mail.ru>
2022-04-13 16:20:55 +0300

    ver 3

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -5,9 +5,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
@@ -16,17 +19,22 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
 
+    public String generateDate(int Days) {
+        return LocalDate.now().plusDays(Days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     void firstTest(){
         Configuration.holdBrowserOpen = true;
 
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").setValue("16.04.2022");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+79991234567");
         $("[data-test-id='agreement']").click();
-        $$("button").find(exactText("Забронировать")).click();
+        $$(withText("Забронировать")).first().click();
+        //$$("button").find(exactText("Забронировать")).click();
         $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(14));
     }
 
