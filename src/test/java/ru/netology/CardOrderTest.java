6e4453c475439af44c5f87fc1903c82d1383cb8c
Victoria Pavlova <pvd0510@mail.ru>
2022-04-13 16:06:33 +0300

    ver 2

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -15,19 +15,6 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @AfterEach
-    void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 
     @Test
     void firstTest(){
@@ -40,6 +27,88 @@ public class CardOrderTest {
         $("[data-test-id='phone'] input").setValue("+79991234567");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(14));
+    }
+
+
+    @Test
+    void testNoName(){
+        Configuration.holdBrowserOpen = true;
+
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").setValue("16.04.2022");
+        $("[data-test-id='phone'] input").setValue("+79991234567");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='name'] .input__sub").should(visible, Duration.ofSeconds(14));
+    }
+
+    @Test
+    void testInvalidName(){
+        Configuration.holdBrowserOpen = true;
+
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").setValue("16.04.2022");
+        $("[data-test-id='name'] input").setValue("Ivanov Ivan");
+        $("[data-test-id='phone'] input").setValue("+79991234567");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='name'] .input__sub").should(visible, Duration.ofSeconds(14));
+    }
+
+    @Test
+    void testNoCity(){
+        Configuration.holdBrowserOpen = true;
+
+        open("http://localhost:9999/");
+        $("[data-test-id='date'] input").setValue("16.04.2022");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79991234567");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='city'] .input__sub").should(visible, Duration.ofSeconds(14));
+    }
+
+    @Test
+    void testInvalidCity(){
+        Configuration.holdBrowserOpen = true;
+
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Moscow");
+        $("[data-test-id='date'] input").setValue("16.04.2022");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79991234567");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='city'] .input__sub").should(visible, Duration.ofSeconds(14));
+    }
+
+    @Test
+    void testNoPhone(){
+        Configuration.holdBrowserOpen = true;
+
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Moscow");
+        $("[data-test-id='date'] input").setValue("16.04.2022");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='phone'] .input__sub").should(visible, Duration.ofSeconds(14));
+    }
+
+    @Test
+    void testInvalidPhone(){
+        Configuration.holdBrowserOpen = true;
+
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Moscow");
+        $("[data-test-id='date'] input").setValue("16.04.2022");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+7999123456");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='phone'] .input__sub").should(visible, Duration.ofSeconds(14));
     }
 }
